d861842451e2287157e0982793fda84926f66c3d
Matt Vertescher <mvertescher@gmail.com>
2021-03-11 18:22:33 -0500

    Add extra vim config

--- nix/vim.nix
+++ nix/vim.nix
@@ -19,6 +19,28 @@
       vim-gitgutter
     ];
 
-    extraConfig = "";
+    extraConfig = ''
+        set colorcolumn=80
+        set cursorline
+        set spell
+        set list
+
+        colorscheme nord
+
+        set hlsearch
+        highlight ExtraWhitespace ctermbg=darkgreen guibg=darkgreen
+
+        " Tab width to two spaces
+        set tabstop=2 shiftwidth=2 expandtab
+
+        " Remove trailing whitespace on write
+        " autocmd BufWritePre * :%s/\s\+$//e
+
+        " Map control-p to skim in normal mode
+        map <C-p> :SK<CR>
+
+        " Open GitMessenger commit popup
+        map <C-w>m :GitMessenger<CR>
+    '';
   };
 }
