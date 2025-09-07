5e65dbfb53d31f0b7ba7cf6ca3bad7cedb4f9bc1
Matt Vertescher <mvertescher@gmail.com>
2025-09-07 14:04:10 -0700

    Sync config

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -7,6 +7,8 @@ in {
     ./binds.nix
   ];
 
+  # home.pointerCursor.hyprcursor.enable = true;
+
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = [ ];
@@ -17,8 +19,8 @@ in {
       gaps_in = 5;
       gaps_out = 20;
       border_size = 2;
-      # col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg;
-      # col.inactive_border = rgba(595959aa);
+      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "col.inactive_border" = "rgba(595959aa)";
       resize_on_border = false;
       allow_tearing = false;
       layout = "dwindle";
@@ -64,7 +66,7 @@ in {
         enabled = true;
         range = 4;
         render_power = 3;
-        # color = rgba(1a1a1aee);
+        color = "rgba(1a1a1aee)";
       };
     };
 
@@ -75,7 +77,39 @@ in {
     # ];
 
     animations = {
-      enabled = 0;
+      enabled = 1;
+
+      # Default curves
+      bezier = [
+        # NAME,          X0,   Y0,   X1,   Y1
+        "easeOutQuint,   0.23, 1,    0.32, 1"
+        "easeInOutCubic, 0.65, 0.05, 0.36, 1"
+        "linear,         0,    0,    1,    1"
+        "almostLinear,   0.5,  0.5,  0.75, 1"
+        "quick,          0.15, 0,    0.1,  1"
+      ];
+
+      # Default animations
+      animation = [
+        # NAME,         ONOFF, SPEED, CURVE,        [STYLE]
+        "global,        1,     10,    default"
+        "border,        1,     5.39,  easeOutQuint"
+        "windows,       1,     4.79,  easeOutQuint"
+        "windowsIn,     1,     4.1,   easeOutQuint, popin 87%"
+        "windowsOut,    1,     1.49,  linear,       popin 87%"
+        "fadeIn,        1,     1.73,  almostLinear"
+        "fadeOut,       1,     1.46,  almostLinear"
+        "fade,          1,     3.03,  quick"
+        "layers,        1,     3.81,  easeOutQuint"
+        "layersIn,      1,     4,     easeOutQuint, fade"
+        "layersOut,     1,     1.5,   linear,       fade"
+        "fadeLayersIn,  1,     1.79,  almostLinear"
+        "fadeLayersOut, 1,     1.39,  almostLinear"
+        "workspaces,    1,     1.94,  almostLinear, fade"
+        "workspacesIn,  1,     1.21,  almostLinear, fade"
+        "workspacesOut, 1,     1.94,  almostLinear, fade"
+        "zoomFactor,    1,     7,     quick"
+      ];
     };
 
     # Ignore maximize requests from apps
