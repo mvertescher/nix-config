99b626381433ffe4f22ac3f7200a1a2ba69274da
Matt Vertescher <mvertescher@gmail.com>
2026-05-17 23:58:52 -0700

    home/host/laptop: add custom cybrcolors theme
    Create a custom Base16 theme 'cybrcolors.yaml' based on the cybrcolors
    lucid color palette. Configure Stylix on laptop to use this custom theme
    instead of synth-midnight-dark.

--- home/host/laptop.nix
+++ home/host/laptop.nix
@@ -13,7 +13,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/tarot.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/synth-midnight-dark.yaml";
+    base16Scheme = ./cybrcolors.yaml;
     image = config.lib.stylix.pixel "base0B";
   };
 
