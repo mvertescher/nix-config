d195fb438d47916d96168d79792310495e9f0b17
Matt Vertescher <mvertescher@gmail.com>
2026-05-18 14:02:46 -0700

    desktop: enable XF86 audio keyboard buttons

--- home/hyprland/binds.nix
+++ home/hyprland/binds.nix
@@ -15,6 +15,13 @@ in {
       "SUPER,mouse:273,resizewindow"
     ];
 
+    bindel = [
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+    ];
+
     bind = let
       workspaces = [
         "0"
