7a7f38dcf5388dd5652ab02b8e615c14e6c6a014
Matt Vertescher <mvertescher@gmail.com>
2026-05-17 22:12:22 -0700

    home: switch laptop styling to synth-midnight-dark base16 scheme

--- home/host/laptop.nix
+++ home/host/laptop.nix
@@ -13,7 +13,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/tarot.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/heetch.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/synth-midnight-dark.yaml";
     image = config.lib.stylix.pixel "base0B";
   };
 
