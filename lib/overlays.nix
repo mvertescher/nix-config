0d19c0cbd8f69610bb6a011ed5073dc92ae14c7e
Matt Vertescher <mvertescher@gmail.com>
2026-07-20 09:05:47 -0700

    lib/overlays: propagate final package set in mkHome builder
    Update mkHome builder in lib/overlays.nix to take final package set p
    instead of unextended fixpoint snapshot f. This ensures overlays
    applied via pkgs.extend are properly available in Home Manager.

--- lib/overlays.nix
+++ lib/overlays.nix
@@ -4,7 +4,7 @@ let
     overlays = f: p: {
         craneLib = inputs.crane.mkLib p;
         builders = {
-            mkHome = { pkgs ? f, extraHomeConfig ? { } }:
+            mkHome = { pkgs ? p, extraHomeConfig ? { } }:
                 import ../outputs/hm.nix { inherit extraHomeConfig inputs pkgs; };
             mkNixos = { pkgs ? f, extraSystemConfig ? { } }:
                 import ../outputs/os.nix { inherit extraSystemConfig inputs pkgs; };
